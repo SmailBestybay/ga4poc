608506e940821377b52efb549ae6f71565d3dc51
Smile <smailbestybay@gmail.com>
2023-09-25 01:37:15 -0700

    rename custom parameter

--- analytics.js
+++ analytics.js
@@ -2,5 +2,5 @@ import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 if (ExecutionEnvironment.canUseDOM) {
   // As soon as the site loads in the browser, register a global event listener
-  gtag("set", { "site version": "1.0" });
+  gtag("set", { site_version: "1.0" });
 }
