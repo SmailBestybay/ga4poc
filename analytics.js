28361ca80fe43b31d704751ada7ffed4aff820b8
eocampo21 <114039611+eocampo21@users.noreply.github.com>
2023-10-03 13:17:23 +0000

    setGtag added

--- analytics.js
+++ analytics.js
@@ -1,5 +1,14 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
+
+export const setGtag = (glueOpsVersionId) => {
+  if (ExecutionEnvironment.canUseDOM) {
+    window.gtag('set', {'glueOpsVersionId': glueOpsVersionId });
+    console.log(`Google Analytics set to: ${glueOpsVersionId}`);
+  }
+};
+
+
 export const logPageView = () => {
   if (ExecutionEnvironment.canUseDOM) {
     // Docusaurus handles page views automatically with the Google Analytics plugin
@@ -15,6 +24,13 @@ export const logPageView = () => {
 export const logEvent = (eventName, eventProperties) => {
   if (ExecutionEnvironment.canUseDOM) {
     // Log a custom event
+    // Add glueOpsVersionId to the event properties
+    const updatedEventProperties = {
+      glueOpsVersionId: 99999999, // Replace with your dynamic value
+    };
+
+    // Set glueOpsVersionId using setGtag
+    setGtag(updatedEventProperties.glueOpsVersionId);
     window.gtag('event', eventName, eventProperties);
     console.log(`Event ${eventName} logged with properties:`, eventProperties);
   }
