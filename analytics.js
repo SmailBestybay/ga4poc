fa60c929bf74816a162ccfd611ff80a5d466f7ac
Smile <smailbestybay@gmail.com>
2023-10-17 17:22:53 -0700

    wip, can't verify if config user_id works

--- analytics.js
+++ analytics.js
@@ -1,48 +1,64 @@
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
-import { setCookie, getClientId } from "./trackUser";
 import pjson from "./package.json";
+import { setCookie, getClientId } from "./trackUser";
+import siteConfig from "@generated/docusaurus.config";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-export const setGtag = (glueOpsVersionId) => {
-  if (ExecutionEnvironment.canUseDOM) {
-    if (typeof window.gtag === "function") {
-      window.gtag("set", { glueOpsVersionId: glueOpsVersionId });
-      console.log(`Google Analytics set to: ${glueOpsVersionId}`);
-    } else {
-      console.warn(
-        "window.gtag is not defined. Google Analytics may not be properly initialized."
-      );
+// Singleton module to make sure is executed only once defined with a IIFE function,
+// which means the function is executed immediately when the script is loaded and only once.
+const LogEventManager = (() => {
+  let clientId;
+  let glueOpsVersion;
+
+  const setup = () => {
+    const days = 365;
+    clientId = getClientId();
+    setCookie("clientId", clientId, days);
+    glueOpsVersion = pjson.version || 8000; // Default to 8000 if not defined
+
+    if (ExecutionEnvironment.canUseDOM) {
+      console.log("IN THE SETUP");
+      window.gtag("config", "G-YJV3SCVC5E", {
+        user_id: clientId,
+      });
+      const id = new Promise((resolve) => {
+        window.gtag("get", "G-YJV3SCVC5E", "user_id", resolve);
+      });
+      id.then((res) => console.log(res));
     }
-  }
-};
+  };
 
-export const logPageView = () => {
-  if (ExecutionEnvironment.canUseDOM) {
-    // Docusaurus handles page views automatically with the Google Analytics plugin
-    // Log a custom event
-    logEvent("log_page_view_event", {
-      event_category: "website loading",
-      event_label: "event_label",
-    });
-    console.log("logPageView");
-  }
-};
+  // Call setup once
+  setup();
+
+  return {
+    get clientId() {
+      return clientId;
+    },
+    get glueOpsVersion() {
+      return glueOpsVersion;
+    },
+  };
+})();
+
+// Possible Button Event Names
+// eventName = purchase_button_clicked_event | git_button_clicked_event
+
+/* Possible eventProperties
+ * {  
+      glueOpsVersion: LogEventManager.glueOpsVersion,
+      clientId: LogEventManager.clientId,
+      event_category: 'Custom Category',
+      event_label: 'Custom Label',
+    })
+ */
 
 export const logEvent = (eventName, eventProperties) => {
   if (ExecutionEnvironment.canUseDOM) {
-    // Generate clientId
-    const clientId = getClientId();
-    setCookie("clientId", clientId, 365);
-
     // Log a custom event
-    // Add glueOpsVersionId to the event properties
-    console.log(pjson.version);
-    console.log(customFields.gluepOpsVersion);
     const updatedEventProperties = {
-      gluepOpsVersion: pjson.version, // Replace with your dynamic value
+      glueOpsSiteVersion: LogEventManager.glueOpsVersion,
+      userID: LogEventManager.clientId,
       ...eventProperties,
-      newPropTest: "newPropTest",
-      anotherNewPropTest: "anotherNewPropTest",
-      cliendId: clientId,
     };
 
     // Check if window.gtag is defined before triggering the event
@@ -50,7 +66,7 @@ export const logEvent = (eventName, eventProperties) => {
       window.gtag("event", eventName, updatedEventProperties);
       console.log(
         `Event ${eventName} logged with properties:`,
-        eventProperties
+        updatedEventProperties
       );
     } else {
       console.warn(
